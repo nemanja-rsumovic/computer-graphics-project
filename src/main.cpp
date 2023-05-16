8243089f4679b193fe09417319b6006dd07c7407
Nemanja Rsumovic <rsumovicnemanja01@gmail.com>
2023-05-16 12:11:15 +0200

    dodat blending i ispravljeni  warnings

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 
 #include <learnopengl/filesystem.h>
 #include <learnopengl/shader.h>
@@ -16,9 +15,9 @@
 
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void framebuffer_size_callback(__attribute__((unused)) GLFWwindow *window, int width, int height);
 
-void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void mouse_callback(__attribute__((unused)) GLFWwindow *window, double xpos, double ypos);
 
 void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
 
@@ -26,11 +25,11 @@ void processInput(GLFWwindow *window);
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
 
-unsigned int loadTexture(char const * path);
+unsigned int loadTexture(char const * path, bool gammaCorrection);
 
 unsigned int loadCubemap(vector<std::string> faces);
 
-void setLights(Shader shader, glm::vec3 pVec[4]);
+void setLights(Shader shader, glm::vec3 pointLightPositions[4]);
 
 
 // settings
@@ -65,16 +64,16 @@ struct ProgramState {
     bool CameraMouseMovementUpdateEnabled = true;
     glm::vec3 backpackPosition = glm::vec3(0.0f);
     float backpackScale = 1.0f;
-    PointLight pointLight;
+    PointLight pointLight{};
     ProgramState()
             : camera(glm::vec3(-2.0f, -50.0f, 10.0f)) {}
 
-    void SaveToFile(std::string filename);
+    void SaveToFile(const std::string& filename) const;
 
-    void LoadFromFile(std::string filename);
+    void LoadFromFile(const std::string& filename);
 };
 
-void ProgramState::SaveToFile(std::string filename) {
+void ProgramState::SaveToFile(const std::string& filename) const {
     std::ofstream out(filename);
     out << clearColor.r << '\n'
         << clearColor.g << '\n'
@@ -88,7 +87,7 @@ void ProgramState::SaveToFile(std::string filename) {
         << camera.Front.z << '\n';
 }
 
-void ProgramState::LoadFromFile(std::string filename) {
+void ProgramState::LoadFromFile(const std::string& filename) {
     std::ifstream in(filename);
     if (in) {
         in >> clearColor.r
@@ -106,7 +105,7 @@ void ProgramState::LoadFromFile(std::string filename) {
 
 ProgramState *programState;
 
-void DrawImGui(ProgramState *programState);
+void DrawImGui(ProgramState *pState);
 
 // globalne promenljive
 bool spotLightTF = false;
@@ -127,8 +126,8 @@ int main() {
 
     // glfw window creation
     // --------------------
-    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-    if (window == NULL) {
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
@@ -172,11 +171,17 @@ int main() {
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
 
+    // Blending
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     // build and compile shaders
     // -------------------------
-    Shader ourShader("resources/shaders/2.model_lighting.vs", "resources/shaders/2.model_lighting.fs");
-    Shader skyboxShader("resources/shaders/skybox.vs", "resources/shaders/skybox.fs");
-    Shader lightingShader("resources/shaders/lights.vs", "resources/shaders/lights.fs");
+    __attribute__((unused)) Shader ourShader(FileSystem::getPath("resources/shaders/model_lighting.vs").c_str(), FileSystem::getPath("resources/shaders/model_lighting.fs").c_str());
+    Shader skyboxShader(FileSystem::getPath("resources/shaders/skybox.vs").c_str(), FileSystem::getPath("resources/shaders/skybox.fs").c_str());
+    Shader lightingShader(FileSystem::getPath("resources/shaders/lights.vs").c_str(), FileSystem::getPath("resources/shaders/lights.fs").c_str());
+    Shader blendingShader( FileSystem::getPath("resources/shaders/blending1.vs").c_str(), FileSystem::getPath("resources/shaders/blending1.fs").c_str());
+
     // load models
     // -----------
     Model ourModel("resources/objects/backpack/backpack.obj", true);
@@ -254,6 +259,35 @@ int main() {
         glm::vec3(20.0f, 5.0f, 0.0f),
     };
 
+    float transparentVertices[] = {
+            // positions         // texture Coords (swapped y coordinates because texture is flipped upside down)
+            0.0f,  0.5f,  0.0f,  0.0f,  0.0f,
+            0.0f, -0.5f,  0.0f,  0.0f,  1.0f,
+            1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
+            0.0f,  0.5f,  0.0f,  0.0f,  0.0f,
+            1.0f, -0.5f,  0.0f,  1.0f,  1.0f,
+            1.0f,  0.5f,  0.0f,  1.0f,  0.0f
+    };
+
+    //transparent VAO
+    unsigned int transparentVAO, transparentVBO;
+    glGenVertexArrays(1, &transparentVAO);
+    glGenBuffers(1, &transparentVBO);
+    glBindVertexArray(transparentVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, transparentVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(transparentVertices), transparentVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)nullptr);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glBindVertexArray(0);
+    vector<glm::vec3> blendings
+            {
+                    glm::vec3(-75.0f, 15.0f, -3.0f),
+                    /*glm::vec3( 50.0f, 30.0f, 35.0f),
+                    glm::vec3( 25.0f, 10.0f, 0.0f) */
+            };
+
     // skybox VAO
     unsigned int skyboxVAO, skyboxVBO;
     glGenVertexArrays(1, &skyboxVAO);
@@ -262,7 +296,7 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)nullptr);
 
     // load textures
     // -------------
@@ -294,12 +328,20 @@ vector<std::string> faces
     ourShader.setInt("material.texture_diffuse1",0);
     ourShader.setInt("material.texture_specular1",1);
 */
+
+
+    unsigned int transparentTexture = loadTexture(FileSystem::getPath("resources/textures/blending.png").c_str(),true);
+    stbi_set_flip_vertically_on_load(true);
+    blendingShader.use();
+    blendingShader.setInt("texture1", 0);
+
+
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window)) {
         // per-frame time logic
         // --------------------
-        float currentFrame = glfwGetTime();
+        auto currentFrame = (float)glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
 
@@ -345,11 +387,29 @@ vector<std::string> faces
 
         // render the loaded model
         glm::mat4 model = glm::mat4(1.0f);
- /*       model = glm::translate(model,programState->backpackPosition); // translate it down so it's at the center of the scene
+ /*       model = glm::translate(model,programState->backpackPosition); // translate it down, so it's at the center of the scene
         model = glm::scale(model, glm::vec3(programState->backpackScale));    // it's a bit too big for our scene, so scale it down
         ourShader.setMat4("model", model);
         ourModel.Draw(ourShader);
 */
+
+        //blending
+        blendingShader.use();
+        blendingShader.setMat4("projection",projection);
+        blendingShader.setMat4("view",view);
+
+        glBindVertexArray(transparentVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, transparentTexture);
+        for (const glm::vec3& blending : blendings)
+        {
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, blending);
+            model = glm::scale(model,glm::vec3(50.0f));
+            blendingShader.setMat4("model", model);
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+        }
+
 
         lightingShader.use();
         lightingShader.setMat4("model", model);
@@ -357,6 +417,7 @@ vector<std::string> faces
         lightingShader.setMat4("projection", projection);
         lightingShader.setInt("blinn", blinn);
 
+        float j = 1.0f;
         for (unsigned int i = 0; i < 5; i++){
             model = glm::mat4(1.0f);
             if (i == 1)
@@ -370,9 +431,11 @@ vector<std::string> faces
             if (i == 0)
                 model = glm::scale(model, glm::vec3(1.2f, 1.2f, 1.2f));
             else
-                model = glm::scale(model, glm::vec3(0.4f*(i+2)/(i+1), 0.4f*(i+2)/(i+1), 0.4f*(i+2)/(i+1)));
+                model = glm::scale(model, glm::vec3(0.4f*(j+2)/(j+1), 0.4f*(j+2)/(j+1), 0.4f*(j+2)/(j+1)));
             lightingShader.setMat4("model", model);
             island.Draw(lightingShader);
+
+            j+=1.0f;
         }
 
          /*   model = glm::mat4(1.0f);
@@ -419,11 +482,12 @@ vector<std::string> faces
 
      // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
-    //glDeleteVertexArrays(1, &cubeVAO);
+
     glDeleteVertexArrays(1, &skyboxVAO);
-   // glDeleteBuffers(1, &cubeVBO);
     glDeleteBuffers(1, &skyboxVAO);
 
+    glDeleteVertexArrays(1, &transparentVAO);
+    glDeleteBuffers(1, &transparentVBO);
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
     glfwTerminate();
@@ -448,7 +512,7 @@ void processInput(GLFWwindow *window) {
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+void framebuffer_size_callback(GLFWwindow __attribute__((unused)) *window, int width, int height) {
     // make sure the viewport matches the new window dimensions; note that width and
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
@@ -456,18 +520,18 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
 
 // glfw: whenever the mouse moves, this callback is called
 // -------------------------------------------------------
-void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
+void mouse_callback(__attribute__((unused)) GLFWwindow *window, double xpos, double ypos) {
     if (firstMouse) {
-        lastX = xpos;
-        lastY = ypos;
+        lastX = (float)xpos;
+        lastY = (float)ypos;
         firstMouse = false;
     }
 
-    float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+    float xoffset = (float)xpos - (float)lastX;
+    float yoffset = (float)lastY - (float)ypos; // reversed since y-coordinates go from bottom to top
 
-    lastX = xpos;
-    lastY = ypos;
+    lastX = (float)xpos;
+    lastY = (float)ypos;
 
     if (programState->CameraMouseMovementUpdateEnabled)
         programState->camera.ProcessMouseMovement(xoffset, yoffset);
@@ -475,11 +539,11 @@ void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
 // ----------------------------------------------------------------------
-void scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
-    programState->camera.ProcessMouseScroll(yoffset);
+void scroll_callback(GLFWwindow __attribute__((unused))*window, double __attribute__((unused))xoffset, double yoffset) {
+    programState->camera.ProcessMouseScroll((float)yoffset);
 }
 
-void DrawImGui(ProgramState *programState) {
+void DrawImGui(ProgramState *pState) {
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
@@ -490,23 +554,23 @@ void DrawImGui(ProgramState *programState) {
         ImGui::Begin("Hello window");
         ImGui::Text("Hello text");
         ImGui::SliderFloat("Float slider", &f, 0.0, 1.0);
-        ImGui::ColorEdit3("Background color", (float *) &programState->clearColor);
-        ImGui::DragFloat3("Backpack position", (float*)&programState->backpackPosition);
-        ImGui::DragFloat("Backpack scale", &programState->backpackScale, 0.05, 0.1, 4.0);
+        ImGui::ColorEdit3("Background color", (float *) &pState->clearColor);
+        ImGui::DragFloat3("Backpack position", (float*)&pState->backpackPosition);
+        ImGui::DragFloat("Backpack scale", &pState->backpackScale, 0.05, 0.1, 4.0);
 
-        ImGui::DragFloat("pointLight.constant", &programState->pointLight.constant, 0.05, 0.0, 1.0);
-        ImGui::DragFloat("pointLight.linear", &programState->pointLight.linear, 0.05, 0.0, 1.0);
-        ImGui::DragFloat("pointLight.quadratic", &programState->pointLight.quadratic, 0.05, 0.0, 1.0);
+        ImGui::DragFloat("pointLight.constant", &pState->pointLight.constant, 0.05, 0.0, 1.0);
+        ImGui::DragFloat("pointLight.linear", &pState->pointLight.linear, 0.05, 0.0, 1.0);
+        ImGui::DragFloat("pointLight.quadratic", &pState->pointLight.quadratic, 0.05, 0.0, 1.0);
         ImGui::End();
     }
 
     {
         ImGui::Begin("Camera info");
-        const Camera& c = programState->camera;
+        const Camera& c = pState->camera;
         ImGui::Text("Camera position: (%f, %f, %f)", c.Position.x, c.Position.y, c.Position.z);
         ImGui::Text("(Yaw, Pitch): (%f, %f)", c.Yaw, c.Pitch);
         ImGui::Text("Camera front: (%f, %f, %f)", c.Front.x, c.Front.y, c.Front.z);
-        ImGui::Checkbox("Camera mouse update", &programState->CameraMouseMovementUpdateEnabled);
+        ImGui::Checkbox("Camera mouse update", &pState->CameraMouseMovementUpdateEnabled);
         ImGui::End();
     }
 
@@ -514,7 +578,7 @@ void DrawImGui(ProgramState *programState) {
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+void key_callback(GLFWwindow *window, int key, int __attribute__((unused))scancode, int action, int __attribute__((unused))mods) {
 
     // spotLight
     if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
@@ -544,32 +608,38 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 
 // utility function for loading a 2D texture from file
 // ---------------------------------------------------
-unsigned int loadTexture(char const * path)
+
+unsigned int loadTexture(char const * path, bool gammaCorrection)
 {
     unsigned int textureID;
     glGenTextures(1, &textureID);
-
     int width, height, nrComponents;
     unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
     if (data)
     {
-        GLenum format;
+        GLenum internalFormat = 0;
+        GLenum dataFormat = 0;
         if (nrComponents == 1)
-            format = GL_RED;
+        {
+            internalFormat = dataFormat = GL_RED;
+        }
         else if (nrComponents == 3)
-            format = GL_RGB;
+        {
+            internalFormat = gammaCorrection ? GL_SRGB : GL_RGB;
+            dataFormat = GL_RGB;
+        }
         else if (nrComponents == 4)
-            format = GL_RGBA;
-
+        {
+            internalFormat = gammaCorrection ? GL_SRGB_ALPHA : GL_RGBA;
+            dataFormat = GL_RGBA;
+        }
         glBindTexture(GL_TEXTURE_2D, textureID);
-        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, (GLint)internalFormat, width, height, 0, dataFormat, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
-
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
         stbi_image_free(data);
     }
     else
@@ -577,7 +647,6 @@ unsigned int loadTexture(char const * path)
         std::cout << "Texture failed to load at path: " << path << std::endl;
         stbi_image_free(data);
     }
-
     return textureID;
 }
 
@@ -665,8 +734,8 @@ void setLights(Shader lightingShader, glm::vec3 pointLightPositions[]) {
     lightingShader.setVec3("spotLight.direction", programState->camera.Front);
     lightingShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
     if(spotLightTF){
-        lightingShader.setVec3("spotLight.diffuse", 100.0f, 100.0f, 100.0f);
-        lightingShader.setVec3("spotLight.specular", 100.0f, 100.0f, 100.0f);
+        lightingShader.setVec3("spotLight.diffuse", 10.0f, 10.0f, 10.0f);
+        lightingShader.setVec3("spotLight.specular", 10.0f, 10.0f, 10.0f);
 
     }
     else{
